b3aaa61fb937d3172885ebe3e86c6600e420d519
SoundKnight <88587357+SoundKnight@users.noreply.github.com>
2021-11-15 15:24:54 -0600

    Se Agregaron los Archivos del E4

---
+++ Bash/E4_Bash/MainScript.sh
@@ -0,0 +1,14 @@
+# --- INTEGRANTES ---
+# Ibrahim Zavala Hernandez
+# Julio Gerardo Cazarez Gonzalez
+# Pedro Saldaña Vazquez
+
+# --- MODULOS USADOS ---
+# getlocal.sh
+# netScan.sh
+# osdetect.sh
+#  portScanner.sh
+
+# --- PROCESO ---
+sh getlocal.sh < cmds.txt
+echo "Fin del Programa"
